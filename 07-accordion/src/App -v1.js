8d7bb67670fe53538becb0360b2fbce200051ca5
Ishwar <ishwarkuri4@gmail.com>
2024-09-22 13:07:40 +0530

    added code for reuse components

--- 07-accordion/src/App -v1.js
+++ 07-accordion/src/App -v1.js
@@ -26,44 +26,20 @@ export default function App() {
 }
 
 function Accordion({ data }) {
-  const [curOpen, setCurOpen] = useState(null);
-
   return (
     <div className="accordion">
       {data.map((e, i) => (
-        <AccordionItem
-          curOpen={curOpen}
-          onOpen={setCurOpen}
-          num={i}
-          title={e.title}
-          key={e.title}
-        >
-          {e.text}
-        </AccordionItem>
+        <AccordionItem num={i} title={e.title} text={e.text} key={e.title} />
       ))}
-
-      <AccordionItem
-        curOpen={curOpen}
-        onOpen={setCurOpen}
-        num={22}
-        title="Test1"
-        key="hey"
-      >
-        <p>Allows React developers to:</p>
-        <ul>
-          <li>Break up UI into components</li>
-          <li>Make components reusuable</li>
-          <li>Place state efficiently</li>
-        </ul>
-      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, curOpen, onOpen, children }) {
-  const isOpen = num === curOpen;
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   function handleToggel() {
-    onOpen(isOpen ? null : num);
+    setIsOpen((prevState) => !prevState);
   }
 
   return (
@@ -71,7 +47,7 @@ function AccordionItem({ num, title, curOpen, onOpen, children }) {
       <p className="number">{num < 9 ? `0${num + 1}` : num}</p>
       <p className="text">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <p className="content-box">{children}</p>}
+      {isOpen && <p className="content-box">{text}</p>}
     </div>
   );
 }
