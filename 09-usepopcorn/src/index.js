450403c2db35e186c62dcc7d4ceb5def4ae04e99
Ishwar <ishwarkuri4@gmail.com>
2024-10-24 11:23:29 +0530

    updated the code

---
+++ 09-usepopcorn/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StartRating from "./StarRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <StartRating maxRating={5} />
+
+    {/* <App /> */}
+  </React.StrictMode>
+);
