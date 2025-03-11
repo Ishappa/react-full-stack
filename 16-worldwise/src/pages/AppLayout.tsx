37b24beda57cce49c7cad6854ea9da3278e20005
Ishappa <ishappa@codilar.com>
2025-03-11 16:45:23 +0530

    initial commit

---
+++ 16-worldwise/src/pages/AppLayout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AppNav from "../components/AppNav";
+export default function PageLayout() {
+  return (
+    <div>
+      <AppNav />
+      App
+    </div>
+  );
+}
---
+++ 16-worldwise/src/pages/PageNotFound.jsx
@@ -0,0 +1,5 @@
+function PageNotFound() {
+  return <h2 style={{ color: "red" }}>Page Not Found !!</h2>;
+}
+
+export default PageNotFound;
