4f16add439ff2bc2c4a8c1168345c77128116eb0
Ishappa <ishappa@codilar.com>
2025-04-06 22:21:54 +0530

    used context api for cities and country

---
+++ 16-worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetchCities() {
+      try {
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert(error.message, ": error while fetching");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("Cities context was used outsides the CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
