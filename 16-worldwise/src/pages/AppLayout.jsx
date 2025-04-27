e4958a9a04d1ed56532ecded6b175a2f3838d4aa
Ishappa <ishappa@codilar.com>
2025-04-27 18:12:11 +0530

    240. Adding Fake Authentication: Protecting a Route

--- 16-worldwise/src/pages/AppLayout.jsx
+++ 16-worldwise/src/pages/AppLayout.jsx
@@ -3,12 +3,15 @@ import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 import Map from "../components/Map";
 import User from "../components/User";
+import ProtectedRoute from "./ProtectedRoute";
 export default function AppLayout() {
   return (
-    <div className={styles.app}>
-      <Sidebar />
-      <Map />
-      <User/>
-    </div>
+    <ProtectedRoute>
+      <div className={styles.app}>
+        <Sidebar />
+        <Map />
+        <User />
+      </div>
+    </ProtectedRoute>
   );
 }
